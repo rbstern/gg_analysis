8d67e78168c33fdaa9e8580a3ef1e85d0fb93482
Rafael Bassi Stern <rbstern@gmail.com>
2019-07-07 01:12:38 -0300

    Added polychoric and polyserial correlations between GG variables and DGI. Also, added a proportional odds model using GG variables to model DGI.

--- code/03-classification.R
+++ code/03-classification.R
@@ -14,17 +14,25 @@ data_roc_all = NULL
 data_acc_all = NULL
 
 # Penalized logistic regression for GG variables
+n = nrow(dt)
 x = dt %>% select(gg_variables) %>% as.matrix()
 y = dt %>% select(resp_var) %>% as.matrix() %>% as.factor()
-aux <- cv.glmnet(x, y, family = "binomial",
-                 keep = TRUE, nfolds=nrow(dt))
-i <- which(aux$lambda == aux$lambda.min)
-preds_gg <- prediction(aux$fit.preval[,i], dt[[resp_var]])
+# Cross validation
+predictions_gg = rep(NA, n)
+for(ii in 1:n)
+{
+  aux <- cv.glmnet(x[-ii,], y[-ii], family = "binomial",
+                   keep = TRUE, nfolds = nrow(x[-ii,]))
+  predictions_gg[ii] = predict(aux, x, s = "lambda.min")[ii]
+  #predictions_gg[ii] = predict(aux, x)[ii]
+}
+# Accuracy and ROC
+preds_gg <- prediction(predictions_gg, dt[[resp_var]])
 auc = performance(preds_gg, measure = "auc")@y.values[[1]]
 t_acc = max(preds_gg@tp[[1]] + preds_gg@tn[[1]])
 t_n = max(preds_gg@tp[[1]] + preds_gg@tn[[1]] + preds_gg@fp[[1]] + preds_gg@fn[[1]])
 roc_gg <- performance(preds_gg, measure = "tpr", x.measure = "fpr") 
-data_roc <- tibble("auc"     = auc,
+data_roc <- tibble("auc"      = auc,
                    "espec"    = roc_gg@x.values[[1]],
                    "sens"     = roc_gg@y.values[[1]],
                    "variable" = resp_var,
@@ -37,8 +45,26 @@ data_acc = tibble(
 )
 data_acc_all %<>% rbind(data_acc)
 
+# 9 x 1 sparse Matrix of class "dgCMatrix"
+# s0
+# beta_3   .          
+# beta_4   0.356176224
+# beta_7  -3.491689850
+# gamma_3  0.917535171
+# gamma_4  .          
+# gamma_7  2.166144158
+# time_3  -0.002545215
+# time_4   .          
+# time_7  -0.216721650
+aux <- cv.glmnet(x, y, family = "binomial",
+                 keep = TRUE, nfolds=nrow(x[-ii,]))
+glmnet(x, y, family = "binomial", lambda = aux$lambda.min)$beta
+
 # Logistic regression based on MoCA
-preds_moca <- prediction(dt$moca_total, dt[[resp_var]])
+predictions_moca = rep(NA, nrow(dt))
+aux = dt %>% glm(dgi_lgl~moca_total, family = "binomial", data=.)
+predictions_moca = predict(aux, dt)
+preds_moca <- prediction(predictions_moca, dt[[resp_var]])
 t_acc = max(preds_moca@tp[[1]] + preds_moca@tn[[1]])
 t_n = max(preds_moca@tp[[1]] + preds_moca@tn[[1]] + preds_moca@fp[[1]] + preds_moca@fn[[1]])
 auc = performance(preds_moca, measure = "auc")@y.values[[1]]
@@ -49,14 +75,16 @@ data_roc <- tibble("auc" = auc,
                    "variable" = resp_var,
                    "covariate"   = "MOCA")
 data_roc_all %<>% rbind(data_roc)
-data_acc = tibble(
-  acc = t_acc/t_n,
-  variable = resp_var,
-  covariate = "MOCA"
+data_acc = rbind(data_acc,
+                 tibble(acc = t_acc/t_n,
+                        variable = resp_var,
+                        covariate = "MOCA")
 )
 data_acc_all %<>% rbind(data_acc)
 
 # ROC curves for DGI
+theme = theme_set(theme_minimal(base_size = 26))
+theme = theme_update(legend.position="top")
 g <- data_roc_all %>%
   filter(variable == "dgi_lgl") %>%
   ggplot()+
@@ -67,11 +95,31 @@ g <- data_roc_all %>%
   ggtitle("ROC curves for predicting the DGI score")
 ggsave("./plots/gg-classify-dgi.pdf", height = 17, width = 14)
 
-gg_roc <- roc(y, preds_gg@predictions[[1]])
-moca_roc <- roc(y, preds_moca@predictions[[1]])
-roc.test(gg_roc, moca_roc, paired = TRUE, alternative = "greater")
-
+# covariate auc
+# GG        0.652
+# MOCA      0.507
 data_roc_all %>% 
   group_by(variable, covariate) %>% 
   summarise(auc_m = mean(auc))
 
+# p-value = 0.08429
+gg_roc <- roc(y, preds_gg@predictions[[1]])
+moca_roc <- roc(y, preds_moca@predictions[[1]])
+roc.test(gg_roc, moca_roc, paired = TRUE, alternative = "greater")
+
+compareROCdep(cbind(preds_gg@predictions[[1]], preds_moca@predictions[[1]]),
+              t(y), method = "auc")
+ 
+# p-value = 0.01866
+gg_acc = data_acc %>% 
+  filter(covariate == "GG") %>% 
+  select(acc) %>% 
+  unlist()
+binom.test(x = round(gg_acc*nrow(dt)), n = nrow(dt))
+
+# p-value = 0.5386
+gg_moca = data_acc %>% 
+  filter(covariate == "MOCA") %>% 
+  select(acc) %>% 
+  unlist()
+binom.test(x = round(gg_moca*nrow(dt)), n = nrow(dt))
--- code/04-rebuttal.R
+++ code/04-rebuttal.R
@@ -1,3 +1,9 @@
+library(Hmisc)
+library(ordinal)
+library(polycor)
+library(tidyverse)
+library(xtable)
+
 dt = read_rds("./data/complete_data.rds")
 gg_variables = c("beta_3", "beta_4", "beta_7",
                  "gamma_3", "gamma_4", "gamma_7",
@@ -7,51 +13,93 @@ gg_variables = c("beta_3", "beta_4", "beta_7",
 # Correlation analysis #
 ########################
 
-test = cor.test(dt$moca_total, dt$dgi, method = "pearson")
-cor_table = c("moca_total", test$estimate, test$p.value)
+# Partial correlations
+# A tibble: 10 x 3
+# 1 beta_3     -0.00222  0.988
+# 2 beta_4     -0.125    0.344
+# 3 beta_7     -0.122    0.334
+# 4 gamma_3     0.242    0.062
+# 5 gamma_4    -0.0624   0.628
+# 6 gamma_7     0.387    0.001
+# 7 time_3     -0.226    0.084
+# 8 time_4     -0.208    0.127
+# 9 time_7     -0.241    0.054
+# 0 moca_total  0.195    0.08 
+
+cor_table = tibble()
+B = 1000
 for(gg_var in gg_variables)
 {
-  test = cor.test(dt[[gg_var]], dt$dgi, method = "pearson")
-  cor_table = rbind(cor_table, c(gg_var, test$estimate, test$p.value))
+  errors = 0
+  this_estimate = polyserial(dt[[gg_var]], dt$dgi)
+  for(ii in 1:B)
+  {
+    new_permut = sample(1:nrow(dt), replace = FALSE)
+    permut_estimate = abs(polyserial(dt[[gg_var]], dt$dgi[new_permut]))
+    if(permut_estimate >= abs(this_estimate)) errors = errors + 1
+  }
+  cor_table = rbind(cor_table, tibble(variable = gg_var, 
+                                      estimate = this_estimate,
+                                      pvalue   = errors/B))
 }
-cor_table %<>% 
-  as.data.frame() %>%
-  as_tibble() %>%
-  select(var = V1,
-         cor,
-         p_value = V3) %>% 
-  mutate(p_value = round(as.numeric(as.character(p_value)), 3),
-         cor = round(as.numeric(as.character(cor)), 3))
+
+B = 100
+errors = 0
+this_estimate = polychor(dt$moca_total, dt$dgi)
+for(ii in 1:B)
+{
+  new_permut = sample(1:nrow(dt), replace = FALSE)
+  permut_estimate = abs(polychor(dt$moca_total, dt$dgi[new_permut]))
+  if(permut_estimate > abs(this_estimate)) errors = errors + 1
+}
+cor_table = rbind(cor_table, tibble(variable = "moca_total", 
+                                    estimate = this_estimate,
+                                    pvalue   = errors/B))
 
 #######################
 # Regression analysis #
 #######################
 
-#beta_7   -3.4  0.00716 ** 
-#gamma_7   7.6  0.00157 **
+# beta_3  -0.430187   0.40434   
+# beta_4  -0.253721   0.72960   
+# beta_7  -2.569858   0.00711 **
+# gamma_3  3.959327   0.03409 * 
+# gamma_4 -3.608890   0.06483 . 
+# gamma_7  5.285219   0.00305 **
+# time_3  -0.031000   0.75142   
+# time_4  -0.005414   0.96586   
+# time_7  -0.112040   0.61301
 dt %>% 
   select(gg_variables, dgi) %>% 
-  lm(dgi~., data = .) %>% 
+  mutate(dgi = ordered(dgi)) %>% 
+  clm(dgi~., family = "binomial", data = .) %>% 
   summary()
 
-#moca_total  0.2124 0.0673
+  # moca_total  0.10922    0.104
 dt %>% 
   select(moca_total, dgi) %>% 
-  lm(dgi~., data = .) %>% 
+  mutate(dgi = ordered(dgi)) %>% 
+  clm(dgi~., data = .) %>% 
   summary()
 
 # GG means
 # beta_3  beta_4  beta_7 gamma_3 gamma_4 gamma_7  time_3  time_4  time_7 
 # 1.74    1.76    2.00    0.68    0.64    0.78    4.47    3.28    2.52 
-dt %>% 
+m_table = dt %>% 
   select(gg_variables) %>% 
   colMeans() %>% 
-  round(2)
+  round(2) %>% 
+  as.matrix() %>% 
+  t() %>% 
+  as.tibble()
 
-# GG deviations
-# beta_3  beta_4  beta_7 gamma_3 gamma_4 gamma_7  time_3  time_4  time_7 
-# 0.53    0.38    0.37    0.16    0.14    0.18    6.21    3.78    2.40 
-dt %>% 
+v_table = dt %>% 
   select(gg_variables) %>% 
   apply(2, sd) %>% 
-  round(2)
+  round(2) %>% 
+  as.matrix() %>% 
+  t() %>% 
+  as.tibble()
+
+rbind(m_table, v_table) %>% 
+  xtable(type = "latex")
